bba5079e7a3ed26b699daf11e916b8d811c1aed8
davi <wangqiang_1988@msn.cn>
2017-11-09 22:35:31 +0800

    VBO: add render for every attribute

--- GLES3.0/VBO/VBOView.h
+++ GLES3.0/VBO/VBOView.h
@@ -10,8 +10,39 @@
 
 @interface VBOView : DvGLESView
 
+
+/**
+ 不使用顶点缓冲区对象绘制图形
+
+ @param vertices <#vertices description#>
+ @param vtxStride <#vtxStride description#>
+ @param numIndices <#numIndices description#>
+ @param indices <#indices description#>
+ */
 - (void) drawPrimitiveWithoutVBOs:(GLfloat *)vertices andVtxStride:(GLint) vtxStride andNumIndices:(GLint) numIndices andIndices:(GLushort*) indices;
 
+
+/**
+ 使用缓冲区对象绘制（顶点属性结构数组）
+
+ @param numVertices <#numVertices description#>
+ @param vtxBuf <#vtxBuf description#>
+ @param vtxStride <#vtxStride description#>
+ @param numIndices <#numIndices description#>
+ @param indices <#indices description#>
+ */
 - (void) drawPrimitiveWithVBOsByNumVertices:(GLint)numVertices andVtxBuf:(GLfloat *)vtxBuf andVtxStride:(GLint)vtxStride andNumIndices:(GLint)numIndices andIndices:(GLushort *)indices;
+
+/**
+ 使用缓冲区对象绘制（数组结构：对每个属性使用一个缓冲区对象绘制）
+
+ @param numVertices <#numVertices description#>
+ @param vtxPosBuf <#vtxPosBuf description#>
+ @param vtxColorBuf <#vtxColorBuf description#>
+ @param vtxStride <#vtxStride description#>
+ @param numIndices <#numIndices description#>
+ @param indices <#indices description#>
+ */
+- (void) drawPrimitiveWithVBOs2ByNumVertices:(GLint)numVertices andVtxPosBuf:(GLfloat *)vtxPosBuf andVtxColorBuf:(GLfloat *)vtxColorBuf andVtxStride:(GLint *)vtxStride andNumIndices:(GLint)numIndices andIndices:(GLushort *)indices;
 
 @end
--- GLES3.0/VBO/VBOView.m
+++ GLES3.0/VBO/VBOView.m
@@ -16,8 +16,6 @@
 
 @implementation VBOView
 
-GLuint vboIds[2];
-
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
@@ -47,8 +45,31 @@ GLuint vboIds[2];
     };
     
     GLushort indices[3] = {0, 1, 2};
-    //[self drawPrimitiveWithoutVBOs:vertices andVtxStride:sizeof(GLfloat) * (VERTEX_POS_SIZE + VERTEX_COLOR_SIZE) andNumIndices:3 andIndices:indices];
-    [self drawPrimitiveWithVBOsByNumVertices:3 andVtxBuf:vertices andVtxStride:sizeof(GLfloat) * (VERTEX_POS_SIZE + VERTEX_COLOR_SIZE) andNumIndices:3 andIndices:indices];
+    
+//    // 不使用顶点缓冲区对象绘制图形
+//    [self drawPrimitiveWithoutVBOs:vertices andVtxStride:sizeof(GLfloat) * (VERTEX_POS_SIZE + VERTEX_COLOR_SIZE) andNumIndices:3 andIndices:indices];
+    
+//    // 使用缓冲区对象绘制（顶点属性结构数组）
+//    [self drawPrimitiveWithVBOsByNumVertices:3 andVtxBuf:vertices andVtxStride:sizeof(GLfloat) * (VERTEX_POS_SIZE + VERTEX_COLOR_SIZE) andNumIndices:3 andIndices:indices];
+    
+    
+    // 对每个属性使用一个换中区对象绘制
+    GLuint vtxStrides[2];
+    vtxStrides[0] = sizeof(GLfloat) * VERTEX_POS_SIZE;
+    vtxStrides[1] = sizeof(GLfloat) * VERTEX_COLOR_SIZE;
+    
+    GLfloat verticePosArray[VERTEX_POS_SIZE * 3] = {
+        -0.5f, 0.5f, 0.0f,
+        -1.0f, -0.5f, 0.0f,
+        0.0f, -0.5f, 0.0f
+    };
+    GLfloat verticeColorArray[VERTEX_COLOR_SIZE * 3] = {
+        1.0f, 0.0f, 0.0f, 1.0f,
+        0.0f, 1.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f, 1.0f
+    };
+    
+    [self drawPrimitiveWithVBOs2ByNumVertices:3 andVtxPosBuf:verticePosArray andVtxColorBuf:verticeColorArray andVtxStride:vtxStrides andNumIndices:3 andIndices:indices];
 }
 
 - (void) drawPrimitiveWithoutVBOs:(GLfloat *)vertices andVtxStride:(GLint) vtxStride andNumIndices:(GLint) numIndices andIndices:(GLushort*) indices {
@@ -73,6 +94,8 @@ GLuint vboIds[2];
 
 - (void) drawPrimitiveWithVBOsByNumVertices:(GLint)numVertices andVtxBuf:(GLfloat *)vtxBuf andVtxStride:(GLint)vtxStride andNumIndices:(GLint)numIndices andIndices:(GLushort *)indices {
     GLuint offset = 0;
+    
+    static GLuint vboIds[2];
     
     if (vboIds[0] == 0 && vboIds[1] == 0) {
         glGenBuffers(2, vboIds);
@@ -104,5 +127,41 @@ GLuint vboIds[2];
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
+
+- (void) drawPrimitiveWithVBOs2ByNumVertices:(GLint)numVertices andVtxPosBuf:(GLfloat *)vtxPosBuf andVtxColorBuf:(GLfloat *)vtxColorBuf andVtxStride:(GLint *)vtxStride andNumIndices:(GLint)numIndices andIndices:(GLushort *)indices {
+    
+    static GLuint vboIds[3];
+
+    if (vboIds[0] == 0 || vboIds[1] == 0 || vboIds[2] == 0) {
+        glGenBuffers(3, vboIds);
+        glBindBuffer(GL_ARRAY_BUFFER, vboIds[0]);
+        glBufferData(GL_ARRAY_BUFFER, vtxStride[0] * numVertices, vtxPosBuf, GL_STATIC_DRAW);
+        
+        glBindBuffer(GL_ARRAY_BUFFER, vboIds[1]);
+        glBufferData(GL_ARRAY_BUFFER, vtxStride[1] * numVertices, vtxColorBuf, GL_STATIC_DRAW);
+        
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIds[2]);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLfloat) * numIndices, indices, GL_STATIC_DRAW);
+    }
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vboIds[0]);
+    glEnableVertexAttribArray(VERTEX_POS_INDEX);
+    glVertexAttribPointer(VERTEX_POS_INDEX, VERTEX_POS_SIZE, GL_FLOAT, GL_FALSE, vtxStride[0], 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vboIds[1]);
+    glEnableVertexAttribArray(VERTEX_COLOR_INDEX);
+    glVertexAttribPointer(VERTEX_COLOR_INDEX, VERTEX_COLOR_SIZE, GL_FLOAT, GL_FALSE, vtxStride[1], 0);
+   
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIds[2]);
+    
+    glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_SHORT, 0);
+    
+    glDisableVertexAttribArray(VERTEX_POS_INDEX);
+    glDisableVertexAttribArray(VERTEX_COLOR_INDEX);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
 
 @end
