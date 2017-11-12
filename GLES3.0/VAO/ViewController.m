ffc45e3a45359035f40b8cab236196fa1e608ebf
davi <wangqiang_1988@msn.cn>
2017-11-12 23:14:21 +0800

    add VAO demo

--- GLES3.0/VAO/ViewController.m
+++ GLES3.0/VAO/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "VAOView.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    VAOView * view = [[VAOView alloc] initWithFrame:screenBound];
+    [self.view addSubview:view];
 }
 
 
